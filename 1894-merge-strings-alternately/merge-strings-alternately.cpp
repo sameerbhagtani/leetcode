328eb7d384da33343eb3e80ff4177488e1dd45df
Sameer Bhagtani <bhagtanisameer@gmail.com>
2026-01-19 12:12:57 +0530

    Time: 4 ms (30.08%) | Memory: 8.4 MB (63.32%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string merged_string(word1.size() + word2.size(), ' ');
+        string mergedString;
+        int maxLength = max(word1.size(), word2.size());
 
-        int i = 0;
-        int j = 0;
-
-        for (int index = 0; index < merged_string.size(); index++) {
-            if ((index % 2 == 0 && i < word1.size()) || j == word2.size()) {
-                merged_string[index] = word1[i];
-                i++;
-            } else {
-                merged_string[index] = word2[j];
-                j++;
+        for (int i = 0; i < maxLength; i++) {
+            if (i < word1.size()) {
+                mergedString += word1[i];
+            }
+            if (i < word2.size()) {
+                mergedString += word2[i];
             }
         }
-
-        return merged_string;
+        return mergedString;
     }
 };
