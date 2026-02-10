aad9d486321100b645658fcefbd4b7398e6996f0
Sameer Bhagtani <bhagtanisameer@gmail.com>
2026-02-10 23:15:51 +0530

    Time: 2 ms (51.36%) | Memory: 9.9 MB (10.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        if (s.size() != t.size())
+            return false;
+
+        unordered_map<char, int> counts;
+
+        for (int i = 0; i < s.size(); i++) {
+            counts[s[i]]++;
+            counts[t[i]]--;
+        }
+
+        for (const auto& it : counts) {
+            if (it.second != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 };
