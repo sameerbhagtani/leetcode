675d17a16743d2236ce27253a78b5ceda69484ad
Sameer Bhagtani <bhagtanisameer@gmail.com>
2026-04-23 20:49:45 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (68.63%) - LeetSync

---
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int, int> freq;
+
+        for (int num : nums)
+            freq[num]++;
+
+        int maxFreq = 0;
+        for (auto& [num, count] : freq)
+            maxFreq = max(maxFreq, count);
+
+        int total = 0;
+        for (auto& [num, count] : freq)
+            if (count == maxFreq)
+                total += count;
+
+        return total;
+    }
+};
